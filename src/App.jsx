63961a9c9403b3e7b8a2fb00abb2d5b9c793deb6
TDomagalski <tomekwebdev@gmail.com>
2022-10-22 14:18:50 +0200

    fix: changed logic to mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,30 @@ function App() {
 			</div>
 			<div id="App_content">
 				<h1>Hello World</h1>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa ea,
+					dolorum voluptas tempora corporis, reprehenderit voluptatibus esse
+					pariatur laboriosam, id animi nesciunt expedita dicta deserunt!
+					Consectetur ducimus similique mollitia libero. Lorem ipsum dolor sit,
+					amet consectetur adipisicing elit. Culpa ea, dolorum voluptas tempora
+					corporis, reprehenderit voluptatibus esse pariatur laboriosam, id
+					animi nesciunt expedita dicta deserunt! Consectetur ducimus similique
+					mollitia libero.Lorem ipsum dolor sit, amet consectetur adipisicing
+					elit. Culpa ea, dolorum voluptas tempora corporis, reprehenderit
+					voluptatibus esse pariatur laboriosam, id animi nesciunt expedita
+					dicta deserunt! Consectetur ducimus similique mollitia libero.Lorem
+					ipsum dolor sit, amet consectetur adipisicing elit. Culpa ea, dolorum
+					voluptas tempora corporis, reprehenderit voluptatibus esse pariatur
+					laboriosam, id animi nesciunt expedita dicta deserunt! Consectetur
+					ducimus similique mollitia libero.Lorem ipsum dolor sit, amet
+					consectetur adipisicing elit. Culpa ea, dolorum voluptas tempora
+					corporis, reprehenderit voluptatibus esse pariatur laboriosam, id
+					animi nesciunt expedita dicta deserunt! Consectetur ducimus similique
+					mollitia libero.Lorem ipsum dolor sit, amet consectetur adipisicing
+					elit. Culpa ea, dolorum voluptas tempora corporis, reprehenderit
+					voluptatibus esse pariatur laboriosam, id animi nesciunt expedita
+					dicta deserunt! Consectetur ducimus similique mollitia libero.
+				</p>
 				<img src={favicon} alt="favicon" />
 			</div>
 		</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,37 @@ import Logo from "../assets/Logo.svg";
 export const Navbar = () => {
 	return (
 		<>
-			<div className="logo">
-				<img src={Logo} alt="TomDom" />
-				<span>TomDom</span>
-			</div>
-			<nav>
-				<ul>
-					<li>
-						<a href="#">test1</a>
-					</li>
-					<li>
-						<a href="#">test2</a>
-					</li>
-					<li>
-						<a href="#">test3</a>
-					</li>
-				</ul>
-			</nav>
+			<header>
+				<div className="logo">
+					<img src={Logo} alt="TomDom" />
+					<h2>TomDom</h2>
+				</div>
+				<nav>
+					<ul>
+						<li>
+							<a href="#">test1</a>
+						</li>
+						<li>
+							<a href="#">test2</a>
+						</li>
+						<li>
+							<a href="#">test3</a>
+						</li>
+						<li>
+							<a href="#">test1</a>
+						</li>
+						<li>
+							<a href="#">test2</a>
+						</li>
+						<li>
+							<a href="#">test3</a>
+						</li>
+					</ul>
+				</nav>
+				<div className="nav_footer">
+					<h6>footer navigation</h6>
+				</div>
+			</header>
 		</>
 	);
 };
